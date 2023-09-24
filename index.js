92ef5089f39833af84964ecc8310712f198fdcfc
cko003 <cko003@bucknell.edu>
2023-09-24 13:50:39 -0400

    movement

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let boundarySize = 40;
+// let boundarySize = 40;
 
 class Boundary {
+  static width = 40;
+  static height = 40;
   constructor({ position }) {
     this.position = position;
     this.width = 40;
@@ -26,7 +28,36 @@ const map = [
   ["-", "-", "-", "-", "-", "-", "-"],
 ];
 
+class Pacman {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 14;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
 const boundaries = [];
+const pacman = new Pacman({
+  position: {
+    x: Boundary.width + Boundary.width / 2,
+    y: Boundary.height + Boundary.height / 2,
+  },
+  velocity: { x: 0, y: 0 },
+});
 
 map.forEach((row, i) => {
   row.forEach((symbol, j) => {
@@ -35,8 +66,8 @@ map.forEach((row, i) => {
         boundaries.push(
           new Boundary({
             position: {
-              x: boundarySize * j,
-              y: boundarySize * i,
+              x: Boundary.width * j,
+              y: Boundary.height * i,
             },
           })
         );
@@ -47,4 +78,55 @@ map.forEach((row, i) => {
   });
 });
 
-boundaries.forEach((boundary) => boundary.draw());
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  boundaries.forEach((boundary) => boundary.draw());
+  pacman.update();
+};
+
+animate();
+
+window.addEventListener("keydown", ({ key }) => {
+  switch (key) {
+    case "w":
+    case "ArrowUp":
+      pacman.velocity.y = -5;
+      break;
+    case "a":
+    case "ArrowLeft":
+      pacman.velocity.x = -5;
+      break;
+    case "s":
+    case "ArrowDown":
+      pacman.velocity.y = 5;
+      break;
+    case "d":
+    case "ArrowRight":
+      pacman.velocity.x = 5;
+      break;
+  }
+  console.log(pacman.velocity);
+});
+
+window.addEventListener("keyup", ({ key }) => {
+  switch (key) {
+    case "w":
+    case "ArrowUp":
+      pacman.velocity.y = 0;
+      break;
+    case "a":
+    case "ArrowLeft":
+      pacman.velocity.x = 0;
+      break;
+    case "s":
+    case "ArrowDown":
+      pacman.velocity.y = 0;
+      break;
+    case "d":
+    case "ArrowRight":
+      pacman.velocity.x = 0;
+      break;
+  }
+  console.log(pacman.velocity);
+});
